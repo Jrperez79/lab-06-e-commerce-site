cd2a08ab1f489ff0feae26a5540ca54edaf7cd2d
Jrperez79 <joelrperez@gmail.com>
2020-07-15 16:10:58 -0700

    updated getCart addToCart clearCart

--- products/products.js
+++ products/products.js
@@ -2,9 +2,14 @@ import steaks from '../data/steak.js';
 import renderSteak from './render-steak.js';
 
 const list = document.getElementById('steaks');
-
+// A loop that runs through all the available steaks
 for (let i = 0; i < steaks.length; i++) {
+    // Selects a steak based on where it is in the loop
     const steak = steaks[i];
+    // Sets it in the DOM
     const dom = renderSteak(steak);
+    // Appends the information to the Element By ID
     list.appendChild(dom);
 }
+
+
