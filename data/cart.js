3bba91a5d59de63c2ab35229701ceeff97084981
Jrperez79 <joelrperez@gmail.com>
2020-07-14 20:15:46 -0700

    updated cart.js

---
+++ data/cart.js
@@ -0,0 +1,15 @@
+const cart = [{
+    id: 'filet-mignon',
+    quantity: 2
+}, {
+    id: 'porterhouse',
+    quantity: 5
+}, {
+    id: 'rib-eye-steak',
+    quantity: 1
+}, {
+    id: 'sirloin-steak',
+    quantity: 10
+}];
+
+export default cart;
